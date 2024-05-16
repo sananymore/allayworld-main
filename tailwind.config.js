6fa7c95e0334a02d28028bc103ea251360dd1802
sananymore <zhludkop@gmail.com>
2024-05-16 11:36:42 +0200

    made a new website version for re:0 season

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./main/*.html",
+    "./main/**/*.html",
+    "./main/*.php",
+    "./main/**/*.php",
+    "./main/assets/images/*.png",
+    "./main/assets/scripts/*.js",
+    "./main/assets/style/*.css"
+  ],
   darkMode: 'class',
-  content: ["./**/*.{html,js,php}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Montserrat', 'sans-serif'],
-      }
-    }
+        'sans': ['Montserrat', 'sans-serif'],
+        'mono': ['Noto Sans Mono', 'monospace'],
+      },
+      screens: {
+        'xs': '375px',
+      },
+    },
   },
   plugins: [],
 }
