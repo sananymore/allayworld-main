266c76c9d88a5c1a7e922ae48fb5169fabaa17cc
sananymore <zhludkop@gmail.com>
2024-05-19 22:36:08 +0200

    Update

--- main/index.php
+++ main/index.php
@@ -1,11 +1,30 @@
 <!DOCTYPE html>
 <html lang="ru">
-<?php include './elements/head.html'; ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#3D7DE0">
+    <title>ЭЛЛЕЙWORLD</title>
+    <meta name="description" content="Твоя полярная звезда среди серверов Minecraft">
+    <meta property="og:url" content="http://allayworld.fun/">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="ЭЛЛЕЙWORLD">
+    <meta property="og:description" content="Твоя полярная звезда среди серверов Minecraft">
+    <meta property="og:image" content="./assets/img/opengraph.png">
+    <meta property="twitter:domain" content="allayworld.fun">
+    <meta property="twitter:url" content="http://allayworld.fun/">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="ЭЛЛЕЙWORLD">
+    <meta name="twitter:description" content="Твоя полярная звезда среди серверов Minecraft">
+    <meta name="twitter:image" content="./assets/img/opengraph.png">
+    <link rel="icon" type="image/png" href="./assets/img/favicon.png">
+    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="./assets/css/main.min.css">
+</head>
 <body class="bg-neutral-100 dark:bg-neutral-950 text-neutral-900 dark:text-neutral-100 overflow-x-hidden">
-    <!-- <?php include './elements/loader.html'; ?> -->
     <?php include './elements/navbar.html'; ?>
-    <div class="mt-64"></div>
-    
-    <div class="mb-64"></div>
 </body>
 </html>
